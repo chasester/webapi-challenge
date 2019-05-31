5ba50d6120b8f23600463d0c82f036cfa2bfd331
Chase Wex <clapingmanly@hotmail.com>
2019-05-31 12:39:49 -0400

    added middle custom middle where to handle validation for all routes

---
+++ utls/validateRoutes.js
@@ -0,0 +1,57 @@
+const validateObject = require("./validateObject");
+const db_action = require("../data/helpers/actionModel");
+const db_project = require("../data/helpers/projectModel");
+
+function validateAction(req, res, next) {
+    let post =
+    {
+        description: req.body.description ? body.description.substring(0,128) : undefined,
+        notes: req.body.notes
+    }
+
+    let str = validateObject(post);
+    if(str !== "") res.status(400).json({error: `missed data: needs ${str}`});
+    req.data = post;
+    next();
+};
+
+function validateProject(req, res, next) {
+    let post =
+    {
+        name: req.body.name,
+        description: req.body.name,
+        completed: false
+    }
+    let str = validateObject(post);
+    if(str !== "") res.status(400).json({error: `missed data: needs ${str}`});
+    req.project = post;
+    next();
+};
+
+function validateActionId(req, res, next) {
+    if(!req.params || !req.params.id || !parseInt(req.params.id) || req.params.id < 1) return res.status(400).json({error: "id is not defined"});
+    db_action.get(req.params.id)
+    .then(result =>
+    {
+        if(!result || result.length==0) return res.status(400).json({error: "this id does not exist"})
+        req.project = result;
+        next();
+    })
+    .catch(error => res.status(500).json({error: error, message: "internal error of data"}) )
+};
+
+function validateProjectId(req, res, next) {
+    if(!req.params || !req.params.id || !parseInt(req.params.id) || req.params.id < 1) return res.status(400).json({error: "id is not defined"});
+    db_project.get(req.params.id)
+    .then(result =>
+    {
+        if(!result || result.length==0) return res.status(400).json({error: "this id does not exist"})
+        req.project = result;
+        next();
+    })
+    .catch(error => res.status(500).json({error: error, message: "internal error of data"}) )
+};
+
+
+
+module.exports = {validateAction, validateProject, validateProjectId, validateActionId};
